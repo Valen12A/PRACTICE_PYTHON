5ef2ae3b697a71df172a12c471408a8bd921c998
valentina <valeavila717@gmail.com>
2024-07-14 01:48:28 -0500

    Excepciones

---
+++ Excepciones/Excepciones4.py
@@ -0,0 +1,14 @@
+dic={'ciudad':'Soacha',
+    'continente':'Africa',
+    'planeta':'Marte',
+    'pais':'España'}
+
+def clave(key, diccionario):
+    if key  in diccionario:
+        
+        if key in diccionario:
+            l1= diccionario [key]
+
+    else: 
+        return f"La clave {key} no esta en el diccionario"
+print(clave(input("Escriba la clave a buscar: "), dic))
